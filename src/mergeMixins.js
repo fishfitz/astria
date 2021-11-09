ccdb1921dd787c47b8ecbb1590ef6e7cf61108e9
Ninja <ninja@ninja>
2021-11-09 10:38:18 +0100

    fix mixins

--- src/mergeMixins.js
+++ src/mergeMixins.js
@@ -1,16 +1,16 @@
-const mergeArrays = (pageArray, mixinArray) => {
+const merge = (pageArray, mixinArray) => {
   if (!pageArray && !mixinArray) return undefined
-  return [...(mixinArray || []), ...(pageArray || [])]
+  return [...(mixinArray || {}), ...(pageArray || {})]
 }
 
 module.exports = (page, mixin) => {
   return {
-    headers: mergeArrays(mixin.headers, page.headers),
-    params: mergeArrays(mixin.params, page.params),
+    headers: merge(mixin.headers, page.headers),
+    params: merge(mixin.params, page.params),
     paramsError: page.paramsError ? page.paramsError : mixin.paramsError,
-    query: mergeArrays(mixin.query, page.query),
+    query: merge(mixin.query, page.query),
     queryError: page.queryError ? page.queryError : mixin.queryError,
-    files: mergeArrays(mixin.files, page.files),
+    files: merge(mixin.files, page.files),
     handle (...params) {
       if (mixin.handle && !page.handle) return mixin.handle(...params)
       if (mixin.handle) mixin.handle(...params)
